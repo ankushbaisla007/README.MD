3a8fee523cab10e9bd32c1f3df6704a3777f73e5
ankushbaisla007 <152299931+ankushbaisla007@users.noreply.github.com>
2025-08-21 10:24:45 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,5 @@
+first= input("Enter your first name : ")
+last= input("Enter your last name : ")
+full_name= first + " " + last
+greetings= "Hello,\t" + full_name + "!  welcome to the program"
+print(greetings) 
